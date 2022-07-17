7d2bfad0e0c3cb80ef9fba82eaccc5623239bc85
安田圭介 <kyasuda2003@gmail.com>
2022-07-16 20:08:51 -0700

    Update build.sh

--- scripts/cell/build.sh
+++ scripts/cell/build.sh
@@ -5,36 +5,27 @@
 
 [[ -f "./~tmp" ]] && rm ./~tmp
 
-echo ""
-echo "  cell p2p streaming project "
-echo ""
-#ls -al ./
-#source <(wget -q -O - https://raw.githubusercontent.com/hiddenclouds-io/cell/main/scripts/build.sh) "$@"
-cd /$HOME
 cat << EOF
 
--------------------------
- [*] begin test cell
--------------------------
+
+ ██████╗███████╗██╗     ██╗     
+██╔════╝██╔════╝██║     ██║     
+██║     █████╗  ██║     ██║     
+██║     ██╔══╝  ██║     ██║     
+╚██████╗███████╗███████╗███████╗
+ ╚═════╝╚══════╝╚══════╝╚══════╝
+By HiddenClouds.IO                                
 
 EOF
+
+echo [*] test cell
+
 cargo test
 
-cat << EOF
--------------------------
- [*] run cell
--------------------------
-EOF
+echo [*] run cell
+
 cargo run  --release -- --help
 
 "$@"
 
-cat << EOF
-
--------------------------
- [*] end test cell
--------------------------
-
-EOF
-
-#ls -la ./target/debug
+echo [*] build completed.
